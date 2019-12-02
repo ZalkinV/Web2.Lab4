225363d92c8b4d29c3902421018e8464084e3833
Zalkin <Zalkin.viktor@mail.ru>
2019-12-03 00:42:21 +0300

    index. Add trying to connect to DB

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 3000;
 const HOST_NAME = "localhost";
 
 const app = express();
-app.listen(PORT, () => {
-    console.log(`Server has been started at http://${HOST_NAME}:${PORT}`);
-});
+
+
+(async function() {
+    try {
+        await mongoose.connect("", {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server has been started at http://${HOST_NAME}:${PORT}`);
+        });
+    } catch (e) {
+        console.error(e.message);
+    }
+})();
 

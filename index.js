0d10f01694bfcc009a555eab3b8a20f237179888
Zalkin <Zalkin.viktor@mail.ru>
2019-12-03 01:09:13 +0300

    index. Add connection to MongoDB

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
-const HOST_NAME = "localhost";
+const APP_URL = "localhost";
+const APP_PORT = 3000;
+
+const DB_URL = "localhost";
+const DB_PORT = 27017;
+
 
 const app = express();
 
 
 (async function() {
     try {
-        await mongoose.connect("", {
+        await mongoose.connect(`mongodb://${DB_URL}:${DB_PORT}`, {
             useNewUrlParser: true,
-            useFindAndModify: false
+            useFindAndModify: false,
+            useUnifiedTopology: true
         });
 
-        app.listen(PORT, () => {
-            console.log(`Server has been started at http://${HOST_NAME}:${PORT}`);
+        app.listen(APP_PORT, () => {
+            console.log(`Server has been started at http://${APP_URL}:${APP_PORT}`);
         });
     } catch (e) {
         console.error(e.message);

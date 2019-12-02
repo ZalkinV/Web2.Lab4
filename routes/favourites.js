3857b784cac0a2c83cb6defe9ba3a1e3c8dd4c07
Zalkin <Zalkin.viktor@mail.ru>
2019-12-03 02:35:19 +0300

    routes.favourites. Add fake GET route

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 
+router.get("/", (req, res) => {
+    res.status(200).send({ cities: ["fake1", "fake2"] });
+});
 
 router.post("/", (req, res) => {
     res.status(200).json(req.body);

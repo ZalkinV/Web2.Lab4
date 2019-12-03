a57de7d21d95254d21b83120f01d4cef08cbb2ff
Zalkin <Zalkin.viktor@mail.ru>
2019-12-04 00:11:05 +0300

    routes/weather. Add logging error and response with error.message instead of error object

--- routes/weather.js
+++ routes/weather.js
@@ -5,9 +5,10 @@ const router = Router();
 router.get("/", async (req, res) => {
   try {
     const cityName = req.query.cityName;
-    res.status(200).send(cityName);
+    res.status(200).json(cityName);
   } catch (error) {
-    res.status(400).send(error);
+    console.error(error);
+    res.status(400).send(error.message);
   }
 });
 
@@ -15,9 +16,10 @@ router.get("/coordinates", async (req, res) => {
   try {
     const lat = req.query.lat;
     const lon = req.query.lon;
-    res.status(200).send({ lat, lon });
+    res.status(200).json({ lat, lon });
   } catch (error) {
-    res.status(400).send(error);
+    console.error(error);
+    res.status(400).json(error.message);
   }
 });
 

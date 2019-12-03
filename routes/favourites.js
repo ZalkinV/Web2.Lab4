5f0d2b18edb61beaaaa32311c4ccd886fbac44f2
Zalkin <Zalkin.viktor@mail.ru>
2019-12-03 22:47:03 +0300

    routes/favourites. Add using favourite service and async/await

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,31 +1,34 @@
 const { Router } = require("express");
 const router = Router();
 
-const Favourite = require("../models/favourites");
+const { getAllFavourites, createFavourite, deleteFavourite } = require("../services/favourites");
 
 
-router.get("/", (req, res) => {
-  Favourite.find()
-    .then(favourites =>
-      res.status(200).json(favourites))
-    .catch(error =>
-      res.status(400).json(error));
+router.get("/", async (req, res) => {
+  try {
+    const favourites = await getAllFavourites();
+    res.status(200).json(favourites);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  Favourite.create({ cityName: req.body.cityName })
-    .then(saved =>
-      res.status(200).json(saved))
-    .catch(error =>
-      res.status(400).json(error));
+router.post("/", async (req, res) => {
+  try {
+    const created = await createFavourite(req.body.cityName);
+    res.status(200).json(created)
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.delete("/", (req, res) => {
-  Favourite.findOneAndDelete({ cityName: req.body.cityName })
-    .then(deleted =>
-      res.status(200).json(deleted))
-    .catch(error =>
-      res.status(400).json(error));
+router.delete("/", async (req, res) => {
+  try {
+    const deleted = await deleteFavourite(req.body.cityName);
+    res.status(200).json(deleted);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 
